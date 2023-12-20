247fa54020820b67d35a470c6697037fa548d279
M Ashwinth Anbu <ashwinth04@gmail.com>
2023-12-20 15:29:48 +0530

    Added responsiveness to About page

--- src/app/home2/page.jsx
+++ src/app/home2/page.jsx
@@ -1,4 +1,4 @@
-'use client'
+'use client';
 
 import { useState } from "react";
 import MemberCard from "../(components)/MemberCard";
@@ -11,86 +11,88 @@ const Home1 = () => {
 
 
   return (
-    <div className="relative bg-text-color w-full h-[2721px] overflow-hidden text-center text-base text-text-color font-image-text">
-      <div className="absolute top-[0px] left-[-26px] w-[101vw] h-[401px] overflow-hidden text-29xl">
+    <div className="relative bg-text-color lg:w-full sm:w-full h-[2721px] overflow-x-hidden text-center text-base text-text-color font-image-text">
+      <div className="absolute top-[0px] left-[-26px] lg:w-[101vw] lg:h-[401px] overflow-hidden text-29xl first">
         <div className="absolute top-[712px] left-[687px] flex flex-row items-start justify-start gap-[16px] opacity-[0]">
           <div className="relative rounded-[50%] bg-black w-[15px] h-[15px]" />
           <div className="relative rounded-[50%] bg-black w-[15px] h-[15px]" />
           <div className="relative rounded-[50%] bg-black w-[15px] h-[15px]" />
         </div>
         <img
-          className="absolute top-[0px] left-[26px] w-[100vw] h-[979px] object-cover"
+          className="absolute top-[0px] left-[26px] w-[100vw] h-[979px] object-cover first-img"
           alt=""
           src="/unsplashi6at9f7mwdq@2x.png"
         />
         <div className="absolute w-[calc(100%_+_3px)] top-[0px] right-[0px] left-[0px] [background:linear-gradient(180.03deg,_rgba(0,_0,_0,_0.5),_rgba(0,_0,_0,_0)_51.48%,_#000)] h-[401px]" />
-        <b className="absolute top-[308px] left-[calc(50%_-_279px)]">
+        <b className="absolute top-[308px] left-[calc(50%_-_279px)] weare">
           We are Technical affairs
         </b>
       </div>
-      <div className="absolute top-[521px] left-[50px] flex flex-row items-center justify-start gap-[166px] text-left text-17xl text-black font-inter">
-        <div className="flex flex-col items-start justify-start gap-[26px]">
-          <div className="relative font-semibold inline-block w-[662px]">
+      <div className="absolute top-[521px] left-[50px] flex lg:flex-row items-center justify-start gap-[166px] text-left lg:text-17xl md:text-4xl text-black font-inter lg:w-[600px] md:w-[325px] about-us">
+        <div className="flex flex-col items-start justify-start gap-[26px] lg:w-[700px] sm:w-[320px]">
+          <div className="relative font-semibold inline-block sm:w-full md:w-[662px] about-title">
             About Us
           </div>
-          <div className="relative text-9xl capitalize inline-block w-[662px]">{`Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, `}</div>
-        </div>
-        <div className="relative w-[510px] h-[183px] text-xs text-text-color font-image-text">
-          <div className="absolute top-[-0.5px] left-[255px] rounded-tl-none rounded-tr-3xs rounded-b-none w-[255px] h-[182.7px] overflow-hidden">
-            <img
-              className="absolute top-[0px] left-[0px] w-[285px] h-[190px] object-cover"
-              alt=""
-              src="/unsplashourqhrte2im@2x.png"
-            />
-            <div className="absolute top-[0px] left-[0px] rounded-tl-none rounded-tr-3xs rounded-b-none [background:linear-gradient(180deg,_rgba(0,_0,_0,_0),_#000)] w-[255px] h-[182.7px]" />
-            <b className="absolute top-[155.9px] left-[17.9px] inline-block w-[81.2px] h-[15.4px]">
-              Hackathons
-            </b>
+          <div class="capitalize inline-block md:w-full lg:w-[600px] lg:text-9xl sm:text-2xl md:text-2xl">
+              Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s
           </div>
-          <div className="absolute top-[-0.5px] left-[0px] rounded-tl-3xs rounded-tr-none rounded-b-none w-[255px] h-[182.7px] overflow-hidden">
+        </div>
+        <div className="relative flex flex-row w-[510px] h-[183px] text-xs text-text-color font-image-text about-image">
+          <div className=" rounded-tl-3xs rounded-tr-none rounded-b-none w-[255px] h-[182.7px] overflow-hidden img-abt">
             <img
-              className="absolute top-[0px] left-[0px] w-[268px] h-[406px] object-cover"
+              className=" w-[268px] h-[406px] object-cover"
               alt=""
               src="/unsplashmxvkwpijals@2x.png"
             />
-            <div className="absolute top-[0px] left-[0px] rounded-tl-3xs rounded-tr-none rounded-b-none [background:linear-gradient(180deg,_rgba(0,_0,_0,_0),_#000)] w-[255px] h-[182.7px]" />
-            <b className="absolute top-[155.9px] left-[17.9px]">Experience</b>
+            <div className="absolute top-[0px] left-[0px] rounded-tl-3xs rounded-tr-none rounded-b-none [background:linear-gradient(180deg,_rgba(0,_0,_0,_0),_#000)] w-[255px] h-[182.7px] gra" />
+            <b className="relative bottom-[40px] w-[81.2px] h-[15.4px]">Experience</b>
+          </div>
+          <div className=" rounded-tl-none rounded-tr-3xs rounded-b-none w-[255px] h-[182.7px] overflow-hidden img-abt">
+            <img
+              className=" w-[285px] h-[190px] object-cover"
+              alt=""
+              src="/unsplashourqhrte2im@2x.png"
+            />
+            <div className="absolute top-[0px] left-[255px] rounded-tl-none rounded-tr-3xs rounded-b-none [background:linear-gradient(180deg,_rgba(0,_0,_0,_0),_#000)] w-[255px] h-[182.7px] gra" />
+            <b className="relative bottom-[40px] left-[20px] w-[81.2px] h-[15.4px]">
+              Hackathons
+            </b>
           </div>
         </div>
       </div>
-      <div className="absolute top-[904px] left-[0px] w-[100vw] overflow-hidden flex flex-col items-center justify-center py-[72px] px-[54px] bg-[url('/frame-37@3x.png')] bg-cover bg-no-repeat bg-[top] text-17xl">
-        <div className="flex flex-row items-center justify-start gap-[213px]">
-          <div className="flex flex-col items-start justify-start gap-[30px]">
-            <b className="relative">Our Journey</b>
-            <div className="w-[551px] flex flex-col items-start justify-start text-left text-xl">
+      <div className="absolute top-[904px] left-[0px] w-[100vw] overflow-hidden flex flex-col items-center justify-center py-[72px] px-[54px] bg-[url('/frame-37@3x.png')] bg-cover bg-no-repeat bg-[top] text-17xl journey">
+        <div className="flex flex-row items-center justify-start gap-[213px] j-content">
+          <div className="flex flex-col items-start justify-start gap-[30px] ">
+            <b className="relative oj">Our Journey</b>
+            <div className="w-[551px] flex flex-col items-start justify-start text-left text-xl select-bar">
               <div className="relative w-[551px] h-[50px] cursor-pointer hover:[background:linear-gradient(90deg,_#acddf9,_#879cfd)] hover:text-black" onClick={() => (setImage('/unsplashmxvkwpijals1@2x.png'))}>
                 <div className="absolute top-[0px] left-[0px] rounded-8xs  w-[551px] h-[50px]" />
-                <div className="absolute top-[12px] left-[45px] capitalize font-medium inline-block w-[373.8px]">
+                <div className="absolute top-[12px] left-[10px] capitalize font-medium inline-block w-[373.8px] ofv">
                   Our first Vashisht in 2019
                 </div>
               </div>
               <div className="relative w-[551px] cursor-pointer hover:[background:linear-gradient(90deg,_#acddf9,_#879cfd)] h-[50px] hover:text-black"  onClick={() => (setImage('/unsplashmxvkwpijals1@2x.png'))}>
                 <div className="absolute top-[0px] left-[0px] rounded-8xs w-[551px] h-[50px]" />
-                <div className="absolute top-[12px] left-[45px] capitalize font-medium inline-block w-[373.8px]">
+                <div className="absolute top-[12px] left-[10px] capitalize font-medium inline-block w-[373.8px] ofv">
                   Our first Vashisht in 2019
                 </div>
               </div>
               <div className="relative w-[551px] h-[50px] cursor-pointer hover:[background:linear-gradient(90deg,_#acddf9,_#879cfd)] hover:text-black"  onClick={() => (setImage('/unsplashmxvkwpijals1@2x.png'))}>
                 <div className="absolute top-[0px] left-[0px] rounded-8xs w-[551px] h-[50px]" />
-                <div className="absolute top-[12px] left-[45px] capitalize font-medium inline-block w-[373.8px]">
+                <div className="absolute top-[12px] left-[10px] capitalize font-medium inline-block w-[373.8px] ofv">
                   Our first Vashisht in 2019
                 </div>
               </div>
               <div className="relative w-[551px] h-[50px] cursor-pointer hover:[background:linear-gradient(90deg,_#acddf9,_#879cfd)] hover:text-black"  onClick={() => (setImage('/unsplashmxvkwpijals1@2x.png'))}>
                 <div className="absolute top-[0px] left-[0px] rounded-8xs w-[551px] h-[50px]" />
-                <div className="absolute top-[12px] left-[45px] capitalize font-medium inline-block w-[373.8px]">
+                <div className="absolute top-[12px] left-[10px] capitalize font-medium inline-block w-[373.8px] ofv">
                   Our first Vashisht in 2019
                 </div>
               </div>
             </div>
           </div>
-          <div className="flex flex-col items-end justify-start gap-[18px] text-left text-9xl">
-            <div className="relative w-[567px] h-[136px] overflow-hidden shrink-0">
+          <div className="flex flex-col items-end justify-start gap-[18px] text-left text-9xl ji">
+            <div className="relative w-[567px] h-[136px] overflow-hidden shrink-0 journey-img">
               <img
                 className="absolute top-[0px] left-[0px] w-[604px] h-[916px] object-cover"
                 alt=""
@@ -98,10 +100,10 @@ const Home1 = () => {
               />
               <div className="absolute top-[0px] left-[0px] [background:linear-gradient(179.91deg,_rgba(0,_0,_0,_0),_rgba(0,_0,_0,_0.2))] w-[567px] h-[136px]" />
             </div>
-            <div className="relative font-medium inline-block w-[567px]">
+            <div className="relative font-medium inline-block w-[567px] eve">
               Event
             </div>
-            <div className="relative text-xl capitalize inline-block w-[568px]">
+            <div className="relative text-xl capitalize inline-block w-[568px] eve">
               Lorem Ipsum is simply dummy text of the printing and typesetting
               industry.
             </div>
@@ -109,7 +111,7 @@ const Home1 = () => {
         </div>
       </div>
       
-      <div className="absolute top-[1325px] left-[calc(50%_-_719px)] overflow-hidden flex flex-col items-start justify-start py-[72px] px-[49px] text-black">
+      <div className="absolute top-[1325px] left-[calc(50%_-_719px)] overflow-hidden flex flex-col items-start justify-start py-[72px] px-[49px] text-black team">
         <div className="flex flex-col items-center justify-start gap-[50px]">
           <div className="flex flex-col items-center justify-start gap-[20px]">
             <div className="rounded-8xs [background:linear-gradient(90deg,_#acddf9,_#879cfd)] w-[102px] flex flex-row items-center justify-center p-2.5 box-border">
@@ -119,8 +121,9 @@ const Home1 = () => {
           </div>
           <div className="flex flex-col items-center justify-start gap-[50px] text-left text-9xl font-inter">
             <div className="flex flex-col items-center justify-start gap-[30px]">
-              <div className="relative capitalize">Faculty Heads & Team lead</div>
-              <div className="flex flex-row items-start justify-start gap-[26px] text-xs font-image-text">
+              <div className="relative capitalize fhtl">Faculty Heads & Team lead</div>
+              <div className="flex flex-row items-start justify-start gap-[26px] text-xs font-image-text divalign">
+              <div className="flex flex-row gap-[26px]">
                 <div className="relative w-[138px] h-[103px]">
                   <div className="absolute top-[0px] left-[0px] w-[138px] h-[103px]">
                     <div className="absolute top-[0px] left-[0px] [background:linear-gradient(90deg,_#acddf9,_#879cfd)] w-[138px] h-[103px]" />
@@ -151,6 +154,8 @@ const Home1 = () => {
                     src="/image-39@2x.png"
                   />
                 </div>
+                </div>
+                <div className="flex flex-row gap-[26px]">
                 <div className="flex flex-col items-center justify-start">
                   <div className="relative w-[138px] h-[103px]">
                     <div className="absolute top-[0px] left-[0px] w-[138px] h-[103px]">
@@ -201,64 +206,45 @@ const Home1 = () => {
                     />
                   </div>
                 </div>
+                </div>
               </div>
             </div>
             <div className="flex flex-col items-center justify-start gap-[30px]">
-              <div className="relative capitalize">Team members</div>
+              <div className="relative capitalize fhtl">Team members</div>
               <div className="flex flex-row items-start justify-start gap-[26px] text-xs font-image-text animate-scrolling">
-                {arr.map((member) => (
-                  <MemberCard name = "G Praneeth Kumar" />
+                {arr.map((member,index) => (
+                  <MemberCard key={index} name = "G Praneeth Kumar" />
                 ))}
               </div>
             </div>
 
-
             <div className="flex flex-col items-center justify-start gap-[30px]">
-              <div className="relative capitalize">
+              <div className="relative capitalize fhtln">
                 <span>Designed and developed with</span>
                 <span className="text-red">❤</span>
                 <span>️ by</span>
               </div>
               <div className="flex flex-row items-start justify-start gap-[20px] text-xs font-image-text animate-scrolling">
-                {arr.map((member) => (
-                    <MemberCard name = "G Praneeth Kumar" />
+                {arr.map((member,index) => (
+                    <MemberCard key={index} name = "G Praneeth Kumar" />
                   ))}
               </div>
             </div>
           </div>
         </div>
       </div>
-      {/* <div className="absolute top-[0px] left-[1px] w-[98vw] h-[72px] text-left text-black font-inter">
-        <div className="absolute w-full top-[0px] right-[0px] left-[0px] bg-gray [backdrop-filter:blur(20px)] h-[72px]" />
-        <div className="absolute top-[14px] left-[1014px] flex flex-row items-center justify-start gap-[38px]">
-          <div className="relative font-medium">Home</div>
-          <div className="relative font-medium">About</div>
-          <div className="relative font-medium">Clubs</div>
-          <div className="rounded-8xs [background:linear-gradient(90deg,_#acddf9,_#879cfd)] flex flex-row items-start justify-start py-2.5 px-3">
-            <div className="relative font-medium">Joy of giving</div>
-          </div>
-        </div>
-        <div className="absolute top-[27px] left-[50px] font-semibold hidden">
-          Tech affairs logo
-        </div>
-        <img
-          className="absolute top-[11px] left-[49px] w-[50px] h-[50px] object-cover"
-          alt=""
-          src="/image-20@2x.png"
-        />
-      </div> */}
-      <div className="absolute bottom-[0px] w-[100vw] overflow-hidden flex flex-col items-start justify-start p-[50px] box-border bg-[url('/footer@3x.png')] bg-cover bg-no-repeat bg-[top]">
+      <div className="absolute bottom-[0px] w-[100vw] flex flex-col items-start justify-start p-[50px] box-border bg-[url('/footer@3x.png')] bg-cover bg-no-repeat bg-[top] foot">
         <div className="flex flex-col items-start justify-start gap-[126px]">
-          <div className="flex flex-row items-start justify-start gap-[579px]">
-            <div className="flex flex-col items-start justify-start gap-[13px]">
-              <div className="relative">Tech affairs IIITDM</div>
+          <div className="flex flex-row items-start justify-start gap-[579px] sample">
+            <div className="flex flex-col items-start justify-start gap-[13px] textdiv">
+              <div className="">Tech affairs IIITDM</div>
               <div className="relative text-left">
                 <p className="m-0">Lorem ipsum text about a paragraph on</p>
                 <p className="m-0">the creative and talented works done by</p>
                 <p className="m-0">tech affair.</p>
               </div>
             </div>
-            <div className="flex flex-row items-start justify-start gap-[120px] text-left font-inter">
+            <div className="flex flex-row items-start justify-start gap-[120px] text-left font-inter links">
               <div className="flex flex-col items-start justify-start gap-[13px]">
                 <div className="relative font-semibold text-center">
                   Students
@@ -289,13 +275,13 @@ const Home1 = () => {
             </div>
           </div>
           <div className="flex flex-col items-start justify-start gap-[11px] text-left font-inter">
-            <div className="relative box-border w-[1340px] h-px border-t-[1px] border-solid border-text-color" />
+            <div className="relative box-border w-[1340px] h-px border-t-[1px] border-solid border-text-color line" />
             <div className="w-[1339px] flex flex-col items-start justify-start p-2.5 box-border">
-              <div className="w-[1329px] flex flex-row items-center justify-start gap-[799px]">
-                <div className="relative">
+              <div className="w-[1329px] flex flex-row items-center justify-start gap-[799px] last">
+                <div className="relative bottom-content">
                   © Technical Affairs IIITDM. All rights reserved.
                 </div>
-                <div className="flex flex-row items-center justify-center gap-[18px]">
+                <div className="flex flex-row items-center justify-center gap-[18px] icons">
                   <img
                     className="relative w-8 h-8 overflow-hidden shrink-0"
                     alt=""
